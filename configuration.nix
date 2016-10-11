7dc1255187fcda994377aba0c0ad4b73760f582b
Daniel van Dorp <daniel@vandorp.biz>
2016-10-11 22:17:51 +0200

    Explicitly define architecture

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,8 @@
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "16.03";
 
+  nixpkgs.system = "i686-linux";
+
   nixpkgs.config.allowUnfree = true;
 
   programs.zsh.enable = true;

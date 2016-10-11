c51ddc9b3b3d80abf2e9ea21898d2b8266398594
Daniel van Dorp <daniel@vandorp.biz>
2016-10-11 22:19:07 +0200

    Upgrade to NixOS 16.09

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@
   };
 
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "16.03";
+  system.stateVersion = "16.09";
 
   nixpkgs.system = "i686-linux";
 
